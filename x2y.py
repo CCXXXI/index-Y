e2d0757c8e9ca975081e61932f6307c945e2e8a6
CCXXXI <ccxxxi@qq.com>
2025-10-25 12:40:14 +0800

    refactor: add fixed function

--- x2y.py
+++ x2y.py
@@ -4,11 +4,20 @@ from pathlib import Path
 from tqdm import tqdm
 
 
+def fixed(content: str) -> str:
+    return content
+
+
 def x2y():
     x, y = Path("X"), Path("Y")
     shutil.rmtree(y)
     for vol in tqdm(list(x.iterdir())):
         shutil.copytree(vol, y / vol.name)
+        for xhtml in (y / vol.name / "OEBPS/Text").glob("*.xhtml"):
+            with open(xhtml, "r", encoding="utf-8") as f:
+                content = f.read()
+            with open(xhtml, "w", encoding="utf-8") as f:
+                f.write(fixed(content))
 
 
 if __name__ == "__main__":
