c15c3b6cbe90043738557113226b7442f4f879db
CCXXXI <ccxxxi@qq.com>
2025-10-25 11:00:12 +0800

    refactor: do not rename ..X to ..Y
    方便比对差异

--- x2y.py
+++ x2y.py
@@ -1,4 +1,3 @@
-import re
 import shutil
 from pathlib import Path
 
@@ -9,7 +8,7 @@ def x2y():
     x, y = Path("X"), Path("Y")
     shutil.rmtree(y)
     for vol in tqdm(list(x.iterdir())):
-        shutil.copytree(vol, y / re.sub("X$", "Y", vol.name))
+        shutil.copytree(vol, y / vol.name)
 
 
 if __name__ == "__main__":
