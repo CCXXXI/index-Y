5770aba0dc98f5a1107ec7df59084ef9eb96f6ce
CCXXXI <ccxxxi@qq.com>
2025-11-05 22:08:53 +0800

    perf: split fixes by vol

--- x2y.py
+++ x2y.py
@@ -4,22 +4,24 @@ from pathlib import Path
 import regex as re
 from tqdm import tqdm
 
-fixes = (
-    ("人型", "人形"),
-    ("叫作", "叫做"),
-    ("安心感", "安全感"),
-    ("开学式", "开学典礼"),
-    ("(?<=[除并]非)是(?!否)", ""),
-    (r"(?<=[\d零一二三四五六七八九十百千万亿兆])圆(?![顶周])", "元"),
-    (
-        "(?<=(因|由于)[^…。？！]*)(?<!特别|机构|良好|丧命|是这样|冲击波|立场上|「暗部」)(之故|所致|的关系|的缘故)",
-        "",
-    ),
-)
+fixes: dict[str, list[tuple[str, str]]] = {
+    "*": [
+        ("人型", "人形"),
+        ("叫作", "叫做"),
+        ("安心感", "安全感"),
+        ("(?<=[除并]非)是(?!否)", ""),
+        (r"(?<=[\d零一二三四五六七八九十百千万亿兆])圆(?![顶周])", "元"),
+        (
+            "(?<=(因|由于)[^…。？！]*)(?<!特别|机构|良好|丧命|是这样|冲击波|立场上|「暗部」)(之故|所致|的关系|的缘故)",
+            "",
+        ),
+    ],
+    "[S1_06]某魔法的禁书目录 06X": [("开学式", "开学典礼")],
+}
 
 
-def fixed(content: str) -> str:
-    for old, new in fixes:
+def fixed(vol: str, content: str) -> str:
+    for old, new in fixes["*"] + (fixes[vol] if vol in fixes else []):
         content = re.sub(old, new, content)
     return content
 
@@ -35,7 +37,7 @@ def x2y():
             with open(file, "r", encoding="utf-8") as f:
                 content = f.read()
             with open(file, "w", encoding="utf-8") as f:
-                f.write(fixed(content))
+                f.write(fixed(vol.name, content))
 
 
 if __name__ == "__main__":
